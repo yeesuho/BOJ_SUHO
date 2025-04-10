5039bdd267ba7a1392ee843395be3e00834a5178
suho <165283596+yeesuho@users.noreply.github.com>
2025-04-11 01:13:03 +0900

    [Bronze V] Title: 사파리월드, Time: 176 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        long a,b;
+        a = sc.nextLong();
+        b = sc.nextLong();
+        System.out.println(Math.abs(a-b));
+    }
+}
