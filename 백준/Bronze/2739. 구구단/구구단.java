566790536d70b169a57dbaeceef8fc04a2ef584e
suho <165283596+yeesuho@users.noreply.github.com>
2025-04-12 13:45:11 +0900

    [Bronze V] Title: 구구단, Time: 192 ms, Memory: 18500 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = 9;
+        for(int i=1;i<=b;i++) {
+            System.out.println(a+" * "+i+" = "+(a*i));
+        }
+    }
+}
