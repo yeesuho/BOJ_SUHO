78515336fe5eeedab645af3c94435ab9a9248253
suho <165283596+yeesuho@users.noreply.github.com>
2025-08-08 00:18:25 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 312 ms, Memory: 23300 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i=1;i<=a;i++){
+            for(int j=0;j<a-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
