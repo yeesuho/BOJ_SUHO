9f8027bc35be6b6b36345ed6576b5b6390ba0f85
suho <165283596+yeesuho@users.noreply.github.com>
2025-08-17 17:10:24 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 4 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.swift
@@ -0,0 +1,13 @@
+let a = readLine()!
+switch a {
+    case "NLCS":
+    print("North London Collegiate School")
+    case "BHA":
+    print("Branksome Hall Asia")
+    case "KIS":
+    print("Korea International School")
+    case "SJA":
+    print("St. Johnsbury Academy")
+    default :
+    break
+}
