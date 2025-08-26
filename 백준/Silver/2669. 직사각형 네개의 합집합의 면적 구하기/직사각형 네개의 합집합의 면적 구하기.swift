bf1348c5fc65ba8534e7b9b1753161164620a18b
suho <165283596+yeesuho@users.noreply.github.com>
2025-08-27 03:14:52 +0900

    [Silver V] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.swift
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.swift
@@ -1,13 +1,20 @@
 import Foundation
-var a = Set<[Int]>()
+var a = Array(repeating: Array(repeating: false, count: 101), count: 101)
 for _ in 1...4 {
-    let b = readLine()!.split(separator: " ").map{Int($0)!}
+    let b = readLine()!.components(separatedBy: " ").map{Int($0)!}
     let x1 = b[0], y1 = b[1], x2 = b[2], y2 = b[3]
-    
     for x in x1..<x2 {
         for y in y1..<y2 {
-            a.insert([x,y])
+            a[x][y] = true
         }
     }
 }
-print(a.count)
+var c = 0
+for x in 0...100 {
+    for y in 0...100 {
+        if a[x][y] {
+            c += 1
+        }
+    }
+}
+print(c)
