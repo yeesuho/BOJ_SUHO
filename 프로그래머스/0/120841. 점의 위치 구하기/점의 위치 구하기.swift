9f25a59b8daea92ca0d8fe89a27eef897010aff8
suho <165283596+yeesuho@users.noreply.github.com>
2025-10-19 22:27:00 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ dot:[Int]) -> Int {
+    if dot[0] > 0 && dot[1] > 0 {
+        return 1
+    } else if dot[0] < 0 && dot[1] > 0 {
+        return 2
+    } else if dot[0] < 0 && dot[1] < 0 {
+        return 3
+    } else if dot[0] > 0 && dot[1] < 0 {
+        return 4
+    }
+    return 0
+}
