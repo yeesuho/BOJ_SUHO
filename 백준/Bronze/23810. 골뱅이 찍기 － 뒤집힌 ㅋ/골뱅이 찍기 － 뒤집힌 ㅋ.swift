33dc81716d0754482ef25939d6df7a8e8b8c0acd
suho <165283596+yeesuho@users.noreply.github.com>
2025-11-09 22:58:03 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 24 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.swift
@@ -0,0 +1,30 @@
+import Foundation
+func loop(_ num:Int) -> Void {
+  for _ in 0..<num {
+    for _ in 0..<num {
+      print("@@@@@", terminator: "")
+    }
+    print()
+  }
+}
+let n = Int(readLine()!)!
+loop(n)
+for _ in 0..<n {
+  for _ in 0..<1 {
+    for _ in 0..<n {
+      print("@", terminator: "")
+    }
+    print()
+  }
+  
+}
+loop(n)
+for _ in 0..<n {
+  for _ in 0..<2 {
+    for _ in 0..<n {
+      print("@", terminator: "")
+    }
+    print()
+  }
+  
+}
