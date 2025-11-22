3993b0794b9092866b7c9c13f7fd358643f9c252
suho <165283596+yeesuho@users.noreply.github.com>
2025-11-22 23:44:42 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.swift
@@ -0,0 +1,10 @@
+import Foundation
+let nab = readLine()!.components(separatedBy: " ").map{Int($0)!}
+let n = nab[0], a = nab[1], b = nab[2]
+if a<b {
+    print("Bus")
+} else if a>b {
+    print("Subway")
+} else {
+    print("Anything")
+}
