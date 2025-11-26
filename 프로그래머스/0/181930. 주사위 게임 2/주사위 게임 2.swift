0a32f47b75564354594fd5284df65973ae8f6a9c
suho <165283596+yeesuho@users.noreply.github.com>
2025-11-26 22:28:12 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ a:Int, _ b:Int, _ c:Int) -> Int {
+    if a != b && b != c && a != c{
+        return a+b+c
+    } else if a == b && b == c && a == c {
+        return (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c)
+    } else {
+        return (a + b + c) * (a*a + b*b + c*c)
+    }
+    return 0
+}
