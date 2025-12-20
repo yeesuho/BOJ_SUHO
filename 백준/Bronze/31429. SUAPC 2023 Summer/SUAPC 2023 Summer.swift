d02b6d9068f8b49d6b2bc2bb28d7aa22df1540f2
suho <165283596+yeesuho@users.noreply.github.com>
2025-12-20 22:50:38 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.swift
@@ -0,0 +1,28 @@
+import Foundation
+let n = Int(readLine()!)!
+switch n {
+    case 1:
+    print(12, 1600)
+    case 2:
+    print(11, 894)
+    case 3:
+    print(11, 1327)
+    case 4:
+    print(10, 1311)
+    case 5:
+    print(9, 1004)
+    case 6:
+    print(9, 1178)
+    case 7:
+    print(9, 1357)
+    case 8:
+    print(8, 837)
+    case 9:
+    print(7, 1055)
+    case 10:
+    print(6, 556)
+    case 11:
+    print(6, 773)
+    default:
+    break
+}
