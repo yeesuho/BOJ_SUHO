18cd22e81b0f256f65721b4aa0909ad2cff6b67d
suho <165283596+yeesuho@users.noreply.github.com>
2025-12-24 17:24:34 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 8 ms, Memory: 79504 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.swift
@@ -0,0 +1,14 @@
+import Foundation
+let s = readLine()!
+switch s {
+ case "SONGDO":
+    print("HIGHSCHOOL")
+ case "CODE":
+    print("MASTER")
+ case "2023":
+    print("0611")
+ case "ALGORITHM":
+    print("CONTEST")
+ default:
+    break
+}
