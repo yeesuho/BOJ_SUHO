df3205aa5343418c76d7ccd2ec975dd72c17e328
suho <165283596+yeesuho@users.noreply.github.com>
2026-01-30 23:28:44 +0900

    [Bronze V] Title: Football Scoring, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/24736. Football Scoring/Football Scoring.swift
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.swift
@@ -1,7 +1,6 @@
 import Foundation
 for _ in 1...2 {
-    let tfspc = readLine()!.components(separatedBy: " ").map{Int($0)!}
-    let t = tfspc[0], f = tfspc[1], s = tfspc[2], p = tfspc[3], c = tfspc[4]
+    let e=readLine()!.components(separatedBy: " ").map{Int($0)!}
+    let t=e[0],f=e[1],s=e[2],p=e[3],c=e[4]
     print(t*6+f*3+s*2+p*1+c*2, terminator: " ")
 }
-
